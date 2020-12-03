2409a9f5a920be32a7fbc01888c37b8395589159
rado <fact_7mg52e@yahoo.co.jp>
2020-12-03 11:44:13 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
-const FibSeq = [];
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-  if (n === 0) {
-    FibSeq.push(0);
-  } else if (n === 1) {
-    FibSeq.push(1);
-  } else {
-    FibSeq.push(FibSeq[n - 1] + FibSeq[n - 2]);
+  if (memo.has(n)) {
+    return memo.get(n);
   }
-  return FibSeq[n];
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value)
+  return value;
 }
-const length = 40;
+const length = 100;
 for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
